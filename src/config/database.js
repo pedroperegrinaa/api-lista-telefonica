7ecd373be1708741e54047963f04b14b4985f77b
pedroperegrinaa <pedrosilva1p333@gmail.com>
2022-02-25 14:36:32 -0300

    na minha maquina funciona

--- src/config/database.js
+++ src/config/database.js
@@ -2,14 +2,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    url: process.env.DATABASE_URL,
     dialect: process.env.DATABASE_DIALECT,
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-
+    database: process.env.DATABASE_DB,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
     define: {
         timestamps: true,
         underscore: true,
