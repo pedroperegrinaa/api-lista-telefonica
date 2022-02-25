c99bbafdad97d886efeb562f766f06f49580b5d7
pedroperegrinaa <pedrosilva1p333@gmail.com>
2022-02-24 21:57:56 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import app from './app.js';
 
-const port = 3003;
+const port = process.env.PORT || 3003;
+
 app.listen(port, () => {
     console.log(`Escutando na porta ${port}`);
     console.log(`CTRL + Clique em http://localhost:${port}`);
