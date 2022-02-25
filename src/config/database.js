77cd536a8051219eda7e3323a05f4f23483f609d
pedroperegrinaa <pedrosilva1p333@gmail.com>
2022-02-25 14:15:33 -0300

    mds funciona logo

--- src/config/database.js
+++ src/config/database.js
@@ -2,14 +2,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    dialect: 'postgres',
+    dialect: process.env.DATABASE_DIALECT,
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
+
     define: {
         timestamps: true,
         underscore: true,
-    }
+    },
+
 };
